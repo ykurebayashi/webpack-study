a6fe964686d5e7f0f240beb8e7458c9d62cf342a
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2024-08-14 19:26:00 -0300

    feat: adds actual laugh image to the html

--- Project - 2 - Traversy Media/src/index.js
+++ Project - 2 - Traversy Media/src/index.js
@@ -2,4 +2,7 @@ import generateJoke from './generateJoke';
 import './styles/main.scss';
 import laugh from './assets/smile.png'
 
+const laughImg = document.getElementById("laughImg");
+laughImg.src = laugh;
+
 console.log(generateJoke())
